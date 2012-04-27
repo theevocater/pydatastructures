228a2654c18ed692480cdbd425ff8f6ea891539b
theevocater <theevocater@gmail.com>
2012-04-26 23:28:22 -0400

    Inital run down of the classes

--- pygraph/graph.py
+++ pygraph/graph.py
@@ -1,9 +1,15 @@
 class Vertex:
-    pass
+    def __init__(self, data):
+        self.data = data
+        self.in_edges = []
+        self.out_edges = []
 
 
 class Edge:
-    pass
+    def __init__(self, data, v1, v2):
+        self.data = data
+        self.v1 = v1
+        self.v2 = v2
 
 
 class Graph:
